60efb57e5aba534402bfe1aa509a71ae487f1d4a
code-hopper <ameykhondekar01@gmail.com>
2025-01-20 13:34:55 +0530

    addded career timeline and recent projects tab in career timeline sectio

--- src/components/sections/MyCareer.jsx
+++ src/components/sections/MyCareer.jsx
@@ -1,199 +1,180 @@
-import React from 'react'
-
-import { GoDotFill } from "react-icons/go";
-import { FaRegDotCircle } from "react-icons/fa";
-import { IoMdArrowDropup } from "react-icons/io";
-import { FaCode } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
 
 const MyCareer = () => {
+    // const tabs = ['tab-2018', 'tab-2020', 'tab-2022', 'tab-2023', 'tab-2024']; // IDs of the tabs
+    // const [activeIndex, setActiveIndex] = useState(0);
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setActiveIndex((prevIndex) => (prevIndex + 1) % tabs.length); // Cycle through tabs
+    //     }, 3000); // Change every 3 seconds
+
+    //     return () => clearInterval(interval); // Cleanup interval on unmount
+    // }, []);
+
+    // useEffect(() => {
+    //     const activeTab = document.getElementById(tabs[activeIndex]);
+    //     if (activeTab) {
+    //         activeTab.click(); // Programmatically click the tab to activate it
+    //     }
+    // }, [activeIndex]);
+
     return (
         <>
-            <div className='container-fluid'>
+            <div className='container-fluid career-timeline-main-container'>
                 <div className="container">
                     <span className='fw-bolder fs-3'>
                         Career Timeline
                     </span>
                 </div>
-                <div className='container py-5 d-flex justify-content-center'>
-                    <div className='career-graph-pointers d-flex align-items-center gap-3'>
-                        <div className='small-points'>
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                        </div>
-                        <div className='milestone'>
-                            <span className='time-stamp'>2018</span>
-                            <FaRegDotCircle className='text-warning' size={"25px"} />
-                            <div className='time-stamp-text'>
-                                <IoMdArrowDropup className='up-icon' />
-                                <span className=''>Started with Web Dev</span>
-                            </div>
-                        </div>
-                        <div className='small-points'>
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                        </div>
-                        <div className='milestone'>
-                            <span className='time-stamp'>2020</span>
-                            <FaRegDotCircle className='text-warning' size={"25px"} />
-                            <div className='time-stamp-text'>
-                                <IoMdArrowDropup className='up-icon' />
-                                <span className=''>Freelancing Development</span>
-                            </div>
-                        </div>
-                        <div className='small-points'>
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                        </div>
-                        <div className='milestone'>
-                            <span className='time-stamp'>2022</span>
-                            <FaRegDotCircle className='text-warning' size={"25px"} />
-                            <div className='time-stamp-text'>
-                                <IoMdArrowDropup className='up-icon' />
-                                <span className=''>MERN Stack Trainer</span>
-                            </div>
-                        </div>
-                        <div className='small-points'>
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                        </div>
-                        <div className='milestone'>
-                            <span className='time-stamp'>2023</span>
-                            <FaRegDotCircle className='text-warning' size={"25px"} />
-                            <div className='time-stamp-text'>
-                                <IoMdArrowDropup className='up-icon' />
-                                <span className=''>Started WebSync Solution</span>
-                            </div>
-                        </div>
-                        <div className='small-points'>
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                        </div>
-                        <div className='milestone'>
-                            <span className='time-stamp'>2024</span>
-                            <FaRegDotCircle className='text-warning' size={"25px"} />
-                            <div className='time-stamp-text'>
-                                <IoMdArrowDropup className='up-icon' />
-                                <span className=''>Linux Admin RHCSA </span>
-                            </div>
-                        </div>
-                        <div className='small-points'>
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                        </div>
-                        <div className='milestone'>
-                            <span className='time-stamp d-flex gap-2 align-items-center'>
-                                <div className="bg-warning rounded-circle position-relative" style={{ width: "30px", height: "30px" }}>
-                                    <i className="text-dark fontawesome-family fa-solid fa-code position-absolute start-50 top-50 translate-middle"></i>
-                                    <FaCode className='text-dark position-absolute start-50 top-50 translate-middle' />
+
+                <div className="container career-timeline-action-container py-5">
+
+                    <div className='row flex-column flex-md-row'>
+                        <div className="col">
+                            <div className='row flex-md-row flex-column gap-3'>
+                                <div className="col time-stamps flex-grow-0">
+                                    {/* Navigation Tabs */}
+                                    <ul className="nav nav-tabs d-flex flex-md-column flex-row justify-content-center gap-4 border-0" id="careerTab" role="tablist">
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="time-stamp-button active"
+                                                id="tab-2018"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#career-2018"
+                                            // aria-selected={activeIndex === 0}
+                                            >
+                                                2018
+                                            </button>
+                                        </li>
+
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="time-stamp-button"
+                                                id="tab-2021"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#career-2021"
+                                                type="button"
+                                            // aria-selected={activeIndex === 1}
+                                            >
+                                                2021
+                                            </button>
+                                        </li>
+
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="time-stamp-button"
+                                                id="tab-2020"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#career-2020"
+                                                type="button"
+                                            // aria-selected={activeIndex === 1}
+                                            >
+                                                2020
+                                            </button>
+                                        </li>
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="time-stamp-button"
+                                                id="tab-2022"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#career-2022"
+                                                type="button"
+                                            // aria-selected={activeIndex === 2}
+                                            >
+                                                2022
+                                            </button>
+                                        </li>
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="time-stamp-button"
+                                                id="tab-2023"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#career-2023"
+                                                type="button"
+                                            // aria-selected={activeIndex === 3}
+                                            >
+                                                2023
+                                            </button>
+                                        </li>
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="time-stamp-button"
+                                                id="tab-2024"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#career-2024"
+                                                type="button"
+                                            // aria-selected={activeIndex === 4}
+                                            >
+                                                2024
+                                            </button>
+                                        </li>
+                                    </ul>
                                 </div>
-                                <span>Code<span className="fw-bolder text-warning">Hopper</span></span>
-                            </span>
-                            <FaRegDotCircle className='text-warning' size={"25px"} />
-                            <div className='time-stamp-text'>
-                                <IoMdArrowDropup className='up-icon' />
-                                <span className=''>Learning Networking</span>
+                                <div className="col time-stamp-content">
+                                    {/* Tab Content */}
+                                    <div className="tab-content" style={{textAlign: 'justify'}} id="careerTabContent">
+                                        <div
+                                            className="tab-pane fade show active"
+                                            id="career-2018"
+                                        >
+                                            <span className='fw-bolder fs-5'>Started with Coding and Tech</span>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At commodi, repudiandae iure odit nulla qui quae expedita praesentium, nam molestias voluptatum adipisci sed nihil, eum quo repellendus! Quos similique laboriosam nisi eveniet praesentium esse? Ipsum est et ut, fugit nisi error? Atque ex quod commodi eaque nesciunt itaque ipsam dolore.</p>
+                                        </div>
+                                        <div
+                                            className="tab-pane fade"
+                                            id="career-2020"
+                                        >
+                                            <p>Career for 2020</p>
+                                        </div>
+                                        <div
+                                            className="tab-pane fade"
+                                            id="career-2021"
+                                        >
+                                            <p>Career for 2021</p>
+                                        </div>
+                                        <div
+                                            className="tab-pane fade"
+                                            id="career-2022"
+                                        >
+                                            <p>Career for 2022</p>
+                                        </div>
+                                        <div
+                                            className="tab-pane fade"
+                                            id="career-2023"
+                                        >
+                                            <p>Career for 2023</p>
+                                        </div>
+                                        <div
+                                            className="tab-pane fade"
+                                            id="career-2024"
+                                        >
+                                            <p>Career for 2024</p>
+                                        </div>
+                                        <div
+                                            className="tab-pane fade"
+                                            id="career-2025"
+                                        >
+                                            <p>Career for 2025</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className='small-points'>
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
-                            <GoDotFill />
+                        <div className="col">
+                            <span className='fw-bolder fs-4'>Recent Projects</span>
+                            <div className='recent-case-studies-grid'>
+                                <div className='py-5 bg-light'> case 1 </div>
+                                <div className='py-5 bg-light'> case 2 </div>
+                                <div className='py-5 bg-light'> case 3 </div>
+                                <div className='py-5 bg-light'> case 4 </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default MyCareer
+export default MyCareer;
